d58ee049dafcdec0706ef716dd68d80c48963b4b
Julien <xoco70@hotmail.com>
2015-11-11 10:14:29 -0600

    More Roles added

--- database/seeds/SentinelRoleSeeder.php
+++ database/seeds/SentinelRoleSeeder.php
@@ -12,17 +12,31 @@ class SentinelRoleSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('roles')->delete();
 
+        DB::table('roles')->truncate();
+
+
+        Sentinel::getRoleRepository()->createModel()->create([
+            'name' => 'SuperAdmin',
+            'slug' => 'superadmin',
+        ]);
+        Sentinel::getRoleRepository()->createModel()->create([
+            'name' => 'Owner',
+            'slug' => 'owner',
+        ]);
+        Sentinel::getRoleRepository()->createModel()->create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+        ]);
         Sentinel::getRoleRepository()->createModel()->create([
             'name' => 'Users',
             'slug' => 'users',
         ]);
-
-        Sentinel::getRoleRepository()->createModel()->create([
-            'name' => 'Admins',
-            'slug' => 'admins',
-        ]);
+//
+//        Sentinel::getRoleRepository()->createModel()->create([
+//            'name' => 'Admins',
+//            'slug' => 'admins',
+//        ]);
 
         $this->command->info('Roles seeded!');
     }
