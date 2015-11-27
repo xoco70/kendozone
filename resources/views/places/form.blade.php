e893e4cc670661589e38ce4cb4e92205ec1ef9c1
julien <xoco70@hotmail.com>
2015-11-27 02:00:38 -0600

    Gettings coord from maps in place creation

--- resources/views/places/form.blade.php
+++ resources/views/places/form.blade.php
@@ -1,25 +1,46 @@
-<div class="form-group">
-    {!!  Form::label('name', trans('crud.name')) !!}
-    {!!  Form::text('name', old('name'), ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!!  Form::label('name', trans('crud.coords')) !!}
-    <div class="map-wrapper locationpicker-default"></div>
-    {!!  Form::hidden('coords',old('coords')) !!}
+<div class="col-md-12">
+    <div class="form-group">
+        {!!  Form::label('name', trans('crud.name')) !!}
+        {!!  Form::text('name', old('name'), ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="form-group">
-    {!!  Form::label('city', trans('crud.city')) !!}
-    {!!  Form::text('city', old('city'), ['class' => 'form-control']) !!}
+<div class="col-md-6">
+    <div class="form-group">
+        {!!  Form::label('latitude', trans('crud.latitude'),'') !!}
+        {!!  Form::text('latitude', old('latitude'), ['class' => 'form-control', 'id' => 'lat',  'disabled' => 'disabled']) !!}
+    </div>
+    <div class="form-group">
+        {!!  Form::label('longitude', trans('crud.longitude'),'') !!}
+        {!!  Form::text('longitude', old('longitude'), ['class' => 'form-control', 'id' => 'lng',  'disabled' => 'disabled']) !!}
+    </div>
+
+    <div class="form-group">
+        {!!  Form::label('countryId', trans('crud.country')) !!}
+        {!!  Form::select('countryId', $countries,old('countryId'), ['class' => 'form-control']) !!} <!-- 484 is Mexico Code -->
+    </div>
+    <div class="form-group">
+        {!!  Form::submit($submitButton, ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+
 </div>
-<div class="form-group">
-    {!!  Form::label('state', trans('crud.state'),'') !!}
-    {!!  Form::text('state', old('state'), ['class' => 'form-control']) !!}
+<div class="col-md-6">
+    <div class="form-group">
+        {!!  Form::label('name', trans('crud.coords')) !!}
+        <div class="map-wrapper locationpicker-default" id="locationpicker-default"></div>
+        {!!  Form::hidden('coords',old('coords')) !!}
+    </div>
 </div>
-<div class="form-group">
-{!!  Form::label('countryId', trans('crud.country')) !!}
-{!!  Form::select('countryId', $countries,old('countryId'), ['class' => 'form-control']) !!} <!-- 484 is Mexico Code -->
-</div>
-<div class="form-group">
-    {!!  Form::submit($submitButton, ['class' => 'btn btn-primary form-control']) !!}
-</div>
+<script>$('#locationpicker-default').locationpicker({
+        location: {latitude: 46.15242437752303, longitude: 2.7470703125},
+        radius: 300,
+        inputBinding: {
+            latitudeInput: $('#lat'),
+            longitudeInput: $('#lng'),
+            radiusInput: $('#us2-radius'),
+            locationNameInput: $('#city')
+        }
+    });
+</script>
+
+
