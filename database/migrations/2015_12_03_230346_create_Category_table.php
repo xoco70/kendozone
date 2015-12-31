03cd045ddd85715ff002105caf2f63f67a6b05b0
julien <xoco70@hotmail.com>
2015-12-31 11:33:11 -0600

    Test

--- database/migrations/2015_12_03_230346_create_Category_table.php
+++ database/migrations/2015_12_03_230346_create_Category_table.php
@@ -40,6 +40,42 @@ class CreateCategoryTable extends Migration
 
 
         });
+
+
+        Schema::create('category_tournament_user', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('category_id')->unsigned()->index();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('category')
+                ->onDelete('cascade');
+
+            $table->integer('tournament_id')->unsigned()->index();
+            $table->foreign('tournament_id')
+                ->references('id')
+                ->on('tournament')
+                ->onDelete('cascade');
+
+            $table
+                ->foreign(array('category_id', 'tournament_id'))
+                ->references(array('category_id', 'tournament_id'))
+                ->on('category_tournament')
+                ->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->boolean('confirmed');
+
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+
+
+        });
     }
 
     /**
@@ -49,7 +85,7 @@ class CreateCategoryTable extends Migration
      */
     public function down()
     {
-
+        Schema::dropIfExists('category_tournament_user');
         Schema::dropIfExists('category_tournament');
         Schema::dropIfExists('category');
 
