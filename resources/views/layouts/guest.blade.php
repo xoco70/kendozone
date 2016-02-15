1095dbf557e03c3c1b27ed0a3ee766212de7939d
Julien <xoco70@hotmail.com>
2016-02-15 12:04:03 -0600

    Now Login / register "work"

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,27 +8,12 @@
 
     <!-- Global stylesheets -->
     {!! Html::style('css/app.css')!!}
+    {!! Html::style('https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900')!!}
 
     {!! Html::style('css/icons/icomoon/styles.css')!!}
-    {{--{!! Html::style('https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900')!!}--}}
-    {{--{!! Html::style('css/bootstrap.css')!!}--}}
-    {{--{!! Html::style('css/core.css')!!}--}}
-    {{--{!! Html::style('css/components.css')!!}--}}
-    {{--{!! Html::style('css/colors.css')!!}--}}
-            <!-- /global stylesheets -->
     {!! Html::script('js/app.js') !!}
 
-    <!-- Core JS files -->
-    {{--{!! Html::script('js/plugins/loaders/pace.min.js') !!}--}}
-    {{--{!! Html::script('js/core/libraries/jquery.min.js') !!}--}}
-    {{--{!! Html::script('js/core/libraries/bootstrap.min.js') !!}--}}
-    {{--{!! Html::script('js/plugins/loaders/blockui.min.js') !!}--}}
-    {{--<!-- /core JS files -->--}}
-    {{--<!-- Theme JS files -->--}}
-{{--    {!! Html::script('js/plugins/forms/styling/uniform.min.js') !!}--}}
-    {{--{!! Html::script('js/core/app.js') !!}--}}
-    {{--{!! Html::script('js/pages/login.js') !!}--}}
-            <!-- /theme JS files -->
+
 
 
 </head>
@@ -63,6 +48,8 @@
             <div class="content">
                 <!-- Advanced login -->
                 @include('layouts.flash')
+                @include('errors.list')
+
                 @yield('content')
                 @include('layouts.footer')
             </div>
