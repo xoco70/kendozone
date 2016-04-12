2ef8bff2418ee5ea9150b9d9ad4d11324b97e500
julien <xoco70@hotmail.com>
2016-04-12 18:54:29 -0500

    fix map in user.show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -78,9 +78,9 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-6 ">
 
-                                    <img src="{{ $user->avatar }}" width="200" height="200"/>
+                                    <img align="center" src="{{ $user->avatar }}" width="200" height="200"/>
                                 </div>
                             </div>
                         </fieldset>
@@ -134,45 +134,13 @@
     </div>
 
 
-    <script>
-
-        $(function () {
-
-            // Setup map
-            function initialize() {
-
-                // Set coordinates
-                var myLatlng = new google.maps.LatLng("{{$user->latitude}}, {{$user->longitude}}");
-
-                // Options
-                var mapOptions = {
-                    zoom: 5,
-                    center: myLatlng
-                };
-
-                // Apply options
-                var map = new google.maps.Map($('.map-basic')[0], mapOptions);
-
-
-                // Add marker
-                var marker = new google.maps.Marker({
-                    position: myLatlng,
-                    map: map
-                });
-
-                // Attach click event
-                google.maps.event.addListener(marker, 'click', function () {
-                    infowindow.open(map, marker);
-                });
-
-            }
-
-            // Initialize map on window load
-            google.maps.event.addDomListener(window, 'load', initialize);
-
-        });
-
-    </script>
-
 @stop
 
+@section('scripts_footer')
+    <script>
+
+        var latitude = "{{ $user->latitude }}";
+        var longitude = "{{ $user->longitude }}";
+    </script>
+    {!! Html::script('js/pages/footer/userShowFooter.js') !!}
+@stop
