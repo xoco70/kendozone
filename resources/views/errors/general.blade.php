2e60694a184a55ca9cd7b70ce99c81adfd416952
julien <xoco70@hotmail.com>
2016-04-12 18:28:29 -0500

    add png to 404 page

--- resources/views/errors/general.blade.php
+++ resources/views/errors/general.blade.php
@@ -15,8 +15,14 @@
 
                     <!-- Error wrapper -->
                     <div class="container-fluid text-center">
-                        <h1 class="error-title">{{$code}}</h1>
-                        <h6 class="text-semibold content-group">{{$message}}</h6>
+                        <h1 class="error-title">
+                            @if ($code ==404)
+                                <img src="/images/errors/404.png" alt="404 Not Found"/>
+                            @else
+                                {{$code}}
+                            @endif
+                        </h1>
+                        <h1 class="text-semibold content-group text-uppercase">{{$message}}</h1>
                         <blockquote class="no-margin">
                             {{$quote}}
                             <footer>{{$author}} ,<cite title="Source Title">{{$source}}</cite></footer>
@@ -29,7 +35,8 @@
                         @if (Auth::check())
                             <div class="row">
                                 <div class="col-sm-6 col-sm-offset-3">
-                                    <a href="{{URL::action('DashboardController@index')}}" class="btn btn-primary btn-block content-group"><i
+                                    <a href="{{URL::action('DashboardController@index')}}"
+                                       class="btn btn-primary btn-block content-group"><i
                                                 class="icon-circle-left2 position-left"></i> Go to dashboard</a>
                                 </div>
 
