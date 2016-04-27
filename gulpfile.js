58e5986c1e5525cd86184687e897ba7f3f2d241f
julien <xoco70@hotmail.com>
2016-04-26 20:15:54 -0500

    dashboard intermediate

--- gulpfile.js
+++ gulpfile.js
@@ -27,16 +27,11 @@ elixir(function (mix) {
         'nunito.css'
     ], 'public/css/app.css');
     mix.scripts([
-        //'icons/icomoon/styles.css',
         'plugins/loaders/pace.min.js',
         'core/libraries/jquery.min.js', // 2.1.4
         'core/libraries/bootstrap.min.js', // v3.3.6
-        // 'plugins/loaders/blockui.min.js',
         'core/app.js',
         'components/noty/js/noty/packaged/jquery.noty.packaged.js',
-        // 'core/libraries/jquery_ui/sliders.min.js',
-        // 'core/libraries/jquery_ui/touch.min.js',
-        // 'plugins/sliders/slider_pips.min.js',
         'plugins/forms/styling/switch.min.js',
         'vendor/jsvalidation/js/jsvalidation.js',
         'components/protip/protip.min.js'
@@ -63,21 +58,14 @@ elixir(function (mix) {
     ], 'public/js/pages/header/tournamentCreate.js');
 
     mix.scripts([
-        //'http://maps.google.com/maps/api/js',
-        // 'plugins/uploaders/fileinput.min.js',
-        // 'pages/uploader_bootstrap.js',
         'plugins/dropzone.js'
     ], 'public/js/pages/header/userCreate.js');
 
     mix.styles([
         'dropzone.css',
-        // 'dz-basic.css'
     ], 'public/css/pages/userCreate.css');
     mix.scripts([
-        // 'https://maps.google.com/maps/api/js',
-        // 'plugins/uploaders/fileinput.min.js',
         'plugins/uploaders/dropzone.js',
-        // 'plugins/pages/uploader_bootstrap.js'
     ], 'public/js/userEdit.js');
 
     mix.scripts([
@@ -85,12 +73,9 @@ elixir(function (mix) {
         'sidebar_detached_sticky_custom.js',
         'plugins/forms/inputs/duallistbox.min.js',
         'plugins/pickers/location/location.js',
-        //'http://maps.google.com/maps/api/js',
         'plugins/pickers/pickadate/picker.js',
         'plugins/pickers/pickadate/picker.date.js',
         'plugins/notifications/sweet_alert.min.js',
-        // 'core/libraries/jquery_ui/interactions.min.js',
-        // 'core/libraries/jquery_ui/touch.min.js',
         'jquery.timepicker.js',
         'plugins/jquery-dateFormat.min.js',
     ], 'public/js/pages/header/tournamentEdit.js');
