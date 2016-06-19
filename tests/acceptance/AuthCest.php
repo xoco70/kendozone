d7fc6b99808c3097c7c7eadbf6833bb2e9377457
julien <xoco70@hotmail.com>
2016-06-18 23:59:09 -0500

    Change FB Pass

--- tests/acceptance/AuthCest.php
+++ tests/acceptance/AuthCest.php
@@ -29,7 +29,7 @@ class AuthCest
         $I->amOnPage('/auth/login');
         $I->click('#fb');
         $I->fillField('email', 'xoco70@hotmail.com');
-        $I->fillField('pass', 'Zee1shoo');
+        $I->fillField('pass', 'C4t3m4c000');
         $I->click('login'); // press???
         $I->see(env('APP_NAME')); // Footer is the only common screen
 
