188ae729f3ac34f2dd331a5f9080db1ddf0c7727
julien <xoco70@hotmail.com>
2016-06-28 13:32:16 -0500

    Frontend Translation OK

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -4,21 +4,21 @@ namespace App\Http\Controllers;
 
 use App;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Session;
 
 
-class LanguageController extends Controller
+    class LanguageController extends Controller
     {
 
-        public function update($locale){
-//            if(Auth::check()){
-//                Auth::user()->locale = $locale;
-//                Auth::user()->save();
-//            }
-//            Lang::setLocale($locale);
-//            app()->setLocale($locale);
-            return back();
+        public function update($locale)
+        {
+            if (Auth::check()) {
+                Auth::user()->locale = $locale;
+                Auth::user()->save();
+            }
+            Session::put('locale', $locale);
+            return redirect()->back();
+
 
         }
     }
--- app/Http/Middleware/LocaleMiddleware.php
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -3,48 +3,28 @@
 namespace App\Http\Middleware;
 
 use App;
-use Auth;
 use Closure;
 use Session;
 
-class LocaleMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
-     * @return mixed
-     */
-
-//    protected $languages = ['en','es'];
-
-
-    public function handle($request, Closure $next)
+    class LocaleMiddleware
     {
+        /**
+         * Handle an incoming request.
+         *
+         * @param  \Illuminate\Http\Request $request
+         * @param  \Closure $next
+         * @return mixed
+         */
 
-//        if(!Session::has('locale'))
-//        {
-//            Session::put('locale', 'en');
-//        }
-//
-//        app()->setLocale(Session::get('locale'));
+        protected $languages = ['en', 'es'];
 
-        $path = $request->path();
-        $lang = null;
-        if (preg_match('@^lang/(\w+)@', $path, $match)) {
-            $lang = $match[1];
-        }
-        if (Auth::check()) {
-            if ($lang == null)
-                app()->setLocale(Auth::user()->locale);
-            else
-                app()->setLocale($lang);
-        } else {
-            if ($lang != null) {
-                app()->setLocale($lang);
+
+        public function handle($request, Closure $next)
+        {
+            if (Session::has('locale') && in_array(Session::get('locale'), $this->languages)) {
+                App::setLocale(Session::get('locale'));
             }
+
+            return $next($request);
         }
-        return $next($request);
     }
-}
