1d7ad23ca2739975ede56785be2a06c605d63671
julien <xoco70@hotmail.com>
2016-07-22 15:15:24 -0500

    TeamPolicy improvement

--- app/Policies/TeamPolicy.php
+++ app/Policies/TeamPolicy.php
@@ -39,71 +39,75 @@ class TeamPolicy
         // Si es un torneo Distrital, el presidente de fed + el presidente de asso + Club
 
 
-        return true;
+        return $this->authorize($user, $tournament);
     }
 
     // You can store a user if you are not a simple user
     public function store(User $user, Tournament $tournament)
     {
-        return true;
+        return $this->authorize($user, $tournament);
     }
 
 
     public function edit(User $user, Tournament $tournament)
     {
-        return ($tournament->user_id == $user->id || $user->isSuperAdmin());
+        return $this->authorize($user, $tournament);
 
     }
 
     public function update(User $user, Tournament $tournament)
     {
-        return ($tournament->user_id == $user->id || $user->isSuperAdmin());
+        return $this->authorize($user, $tournament);
     }
 
     public function destroy(User $user, Tournament $tournament)
     {
-        return ($tournament->user_id == $user->id || $user->isSuperAdmin());
+        return $this->authorize($user, $tournament);
     }
 
 
     private function authorize(User $user, Tournament $tournament)
     {
         $internationUser = [config('constants.ROLE_FEDERATION_PRESIDENT')];
-        $nationalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT')];
-        $regionalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT')];
-        $stateUser =     [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT'),config('constants.ROLE_CLUB_PRESIDENT')];
-        $municipalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT'),config('constants.ROLE_CLUB_PRESIDENT')];
-        $districtalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT'),config('constants.ROLE_CLUB_PRESIDENT')];
-        $localUser =      [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT'),config('constants.ROLE_CLUB_PRESIDENT')];
-        $noLevelUser =      [config('constants.ROLE_FEDERATION_PRESIDENT'),config('constants.ROLE_ASSOCIATION_PRESIDENT'),config('constants.ROLE_CLUB_PRESIDENT')];
+        $nationalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT')];
+        $regionalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT')];
+        $stateUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT'), config('constants.ROLE_CLUB_PRESIDENT')];
+        $municipalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT'), config('constants.ROLE_CLUB_PRESIDENT')];
+        $districtalUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT'), config('constants.ROLE_CLUB_PRESIDENT')];
+        $localUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT'), config('constants.ROLE_CLUB_PRESIDENT')];
+        $noLevelUser = [config('constants.ROLE_FEDERATION_PRESIDENT'), config('constants.ROLE_ASSOCIATION_PRESIDENT'), config('constants.ROLE_CLUB_PRESIDENT')];
 
-
+// Como puedo saber si esta federacion participa???
         switch (true) {
             case $tournament->isInternational():
-                if (!in_array($user->role_id,$internationUser )) return false;
+                if (!in_array($user->role_id, $internationUser)) return false;
                 break;
             case $tournament->isNational():
-                if (!in_array($user->role_id,$nationalUser )) return false;
+                if (!in_array($user->role_id, $nationalUser)) return false;
                 break;
             case $tournament->isRegional():
-                if (!in_array($user->role_id,$regionalUser )) return false;
+                if (!in_array($user->role_id, $regionalUser)) return false;
                 break;
             case $tournament->isEstate():
-                if (!in_array($user->role_id,$stateUser )) return false;
+                if (!in_array($user->role_id, $stateUser)) return false;
                 break;
             case $tournament->isMunicipal():
-                if (!in_array($user->role_id,$municipalUser )) return false;
+                if (!in_array($user->role_id, $municipalUser)) return false;
                 break;
             case $tournament->isDistrictal():
-                if (!in_array($user->role_id,$districtalUser )) return false;
+                if (!in_array($user->role_id, $districtalUser)) return false;
                 break;
             case $tournament->isLocal():
-                if (!in_array($user->role_id,$localUser )) return false;
+                if (!in_array($user->role_id, $localUser)) return false;
                 break;
             case $tournament->hasNoLevel():
-
+                if (!$tournament->competitors->contains($user)) return false;
                 break;
+            default:
+                return false;
+
         }
+        return true;
     }
 
 }
