cd6863e9330691c0a50bc47fcdb6da79c66213dd
julien <xoco70@hotmail.com>
2016-10-19 12:10:06 -0500

    Add Git Sha to sentry

--- config/sentry.php
+++ config/sentry.php
@@ -4,5 +4,5 @@ return array(
     'dsn' => env('SENTRY_DSN_PRIVATE'),
 
     // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
 );
