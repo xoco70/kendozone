f64c09f55ae0bd7611ca961e9542f39b0160d6d2
Julien Cappiello <xoco70@hotmail.com>
2016-11-23 19:39:50 -0600

    enable active book in left menu

--- resources/views/layouts/displayMyEntityOnLeftSide.blade.php
+++ resources/views/layouts/displayMyEntityOnLeftSide.blade.php
@@ -1,26 +1,29 @@
+<?php
+$isActive = Request::is('federations*') || Request::is('associations*') || Request::is('clubs*') || Request::is('users*');
+?>
 @if (Auth::user()->isFederationPresident() && Auth::user()->federationOwned!=null)
-    <li>
+    <li {{ ($isActive ? 'class=active' : '') }}>
         <a class="protip" data-pt-title="{{ Auth::user()->federationOwned->name }}"
            href="{{  URL::action('FederationController@edit', Auth::user()->federationOwned->id ) }}">
             <i class="icon-address-book position-left sidemenu"></i><span>{{ trans('core.my_federation') }}</span>
         </a>
     </li>
 @elseif (Auth::user()->isAssociationPresident() && Auth::user()->associationOwned!=null)
-    <li>
+    <li {{ ($isActive ? 'class=active' : '') }}>
         <a class="protip" data-pt-title="{{ Auth::user()->associationOwned->name  }}"
            href="{{  URL::action('AssociationController@edit', Auth::user()->associationOwned->id ) }}">
             <i class="icon-address-book position-left sidemenu"></i><span>{{ trans('core.my_association') }}</span>
         </a>
     </li>
 @elseif (Auth::user()->isClubPresident() && Auth::user()->clubOwned!=null)
-    <li>
+    <li {{ ($isActive ? 'class=active' : '') }}>
         <a class="protip" data-pt-title="{{ Auth::user()->clubOwned->name  }}"
            href="{{  URL::action('ClubController@edit', Auth::user()->clubOwned->id ) }}">
             <i class="icon-address-book position-left sidemenu"></i><span>{{ trans('core.my_club') }}</span>
         </a>
     </li>
 @elseif(Auth::user()->isSuperAdmin())
-    <li>
+    <li {{ ($isActive ? 'class=active' : '') }}>
         <a class="protip" data-pt-title="{{ trans_choice('core.federation',2)  }}"
            href="{{  route('federations.index') }}">
             <i class="icon-address-book position-left sidemenu"></i><span>{{ trans_choice('core.federation',2)  }}</span>
