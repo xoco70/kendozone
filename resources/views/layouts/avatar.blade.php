e0c2d6644d1d202b36c3172342ce2b0faf166466
Julien Cappiello <xoco70@hotmail.com>
2017-01-05 21:47:06 -0600

    bug fix

--- resources/views/layouts/avatar.blade.php
+++ resources/views/layouts/avatar.blade.php
@@ -1,3 +1,8 @@
-
-<img src="{!! Auth::getUser()->avatar ?? Avatar::create(Auth::getUser()->email)->toBase64() !!}"
-     alt="kendozone_avatar">
+<?php
+if (Auth::getUser() != null && Auth::getUser()->avatar != null) {
+    $img = Auth::getUser()->avatar;
+} else {
+    $img = Avatar::create(Auth::getUser()->email)->toBase64();
+}
+?>
+<img src="{!! $img !!}" alt="kendozone_avatar">
