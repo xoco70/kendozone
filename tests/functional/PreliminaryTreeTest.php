8cf936289ccf636e221164c3c947dd6f3f2b5387
julien <julien@t4b.mx>
2017-01-28 18:55:58 -0600

    refactor tests with areas

--- tests/functional/PreliminaryTreeTest.php
+++ tests/functional/PreliminaryTreeTest.php
@@ -24,167 +24,86 @@ class PreliminaryTreeTest extends TestCase
         $this->root = factory(User::class)->create(['role_id' => Config::get('constants.ROLE_SUPERADMIN')]);
         $this->logWithUser($this->root);
 
-        $this->tournament = factory(Tournament::class)->create();
-        $this->championship = factory(Championship::class)->create([
-            'tournament_id' => $this->tournament->id,
-            'category_id' => 1,
-        ]);
-
-        factory(ChampionshipSettings::class)->create([
-            'championship_id' => $this->championship->id,
-            'hasPreliminary' => 1,
-            'teamSize' => null,
-            'fightingAreas' => 1,
-            'preliminaryWinner' => 1,
-            'preliminaryGroupSize' => 3,
-        ]);
-
     }
 
     /** @test */
-    public function it_create_a_tree_with_1_competitors()
+    public function check_number_of_row_when_generating_tournament()
     {
-        $this->unique_test(1, 0);
+        $competitorsInTree = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+        $numGroupsExpected = [0, 1, 1, 2, 2, 2, 4, 4, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8];
+        $numAreas = [1,2,4];
+//        $preliminarySizeGroup = 3;
+        foreach ($numAreas as $numArea) {
+            foreach ($competitorsInTree as $numCompetitors) {
+                $tournament = factory(Tournament::class)->create();
+
+                $championship = factory(Championship::class)->create([
+                    'tournament_id' => $tournament->id,
+                    'category_id' => 1,
+                ]);
+
+                factory(ChampionshipSettings::class)->create([
+                    'championship_id' => $championship->id,
+                    'hasPreliminary' => 1,
+                    'teamSize' => null,
+                    'fightingAreas' => $numArea,
+                    'preliminaryWinner' => 1,
+                    'preliminaryGroupSize' => 3,
+                ]);
+
+
+                $users = factory(User::class, $numCompetitors)->create(['role_id' => Config::get('constants.ROLE_USER')]);
+                if ($users instanceof User) {
+                    $user = $users;
+                    $users = new Collection();
+                    $users->push($user);
+                }
+                $this->makeCompetitors($championship, $users);
+                $this->generatePreliminaryTree($tournament);
+                for ($area = 1; $area <= $numArea; $area++){
+                    $count = Tree::where('championship_id', $championship->id)
+                                    ->where('area', $area)->count();
+
+
+                    if ((int ) ($numCompetitors / $numArea) <= 1) {
+                        $this->assertTrue($count == 0);
+                    } else {
+                        $expected = (int)($numGroupsExpected[$numCompetitors - 1] / $numArea);
+
+                        if ($count != $expected) {
+                            dd(["NumCompetitors" => $numCompetitors],
+                                ["NumArea" => $numArea],
+                                ["Real" => $count],
+                                ["Excepted" => $expected],
+                                ["numGroupsExpected[".($numCompetitors - 1)."]" => $numGroupsExpected[$numCompetitors - 1]." / ".  $numArea]);
+                        }
+                        $this->assertTrue($count == $expected);
+                    }
+                }
+
+            }
+        }
+
     }
 
-    /** @test */
-    public function it_create_a_tree_with_2_competitors()
-    {
-        $this->unique_test(2, 1);
-
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_3_competitors()
-    {
-        $this->unique_test(3, 1);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_4_competitors()
-    {
-        $this->unique_test(4, 2);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_5_competitors()
-    {
-        $this->unique_test(5, 2);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_6_competitors()
-    {
-
-        $this->unique_test(6, 2);
-
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_7_competitors()
-    {
-        $this->unique_test(7, 4); // Should be reparted 2, 2, 2, 1 not 3,3,1,0
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_8_competitors()
-    {
-        $this->unique_test(8, 4);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_9_competitors()
-    {
-        $this->unique_test(9, 4);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_10_competitors()
-    {
-        $this->unique_test(10, 4);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_11_competitors()
-    {
-        $this->unique_test(11, 4);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_12_competitors()
-    {
-        $this->unique_test(12, 4);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_13_competitors()
-    {
-        $this->unique_test(13, 8);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_14_competitors()
-    {
-        $this->unique_test(14, 8);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_15_competitors()
-    {
-        $this->unique_test(15, 8);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_16_competitors()
-    {
-        $this->unique_test(16, 8);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_17_competitors()
-    {
-        $this->unique_test(17, 8);
-    }
-
-    /** @test */
-    public function it_create_a_tree_with_18_competitors()
-    {
-        $this->unique_test(18, 8);
-    }
 
     /**
      * @param $users
      */
-    public function makeCompetitors($users)
+    public function makeCompetitors($championship, $users)
     {
         foreach ($users as $user) {
             factory(Competitor::class)->create([
                 'user_id' => $user->id,
-                'championship_id' => $this->championship->id,
+                'championship_id' => $championship->id,
                 'confirmed' => 1]);
         }
     }
 
-    public function generatePreliminaryTree()
+    public function generatePreliminaryTree(Tournament $tournament)
     {
-        $this->visit('/tournaments/' . $this->tournament->slug . "/edit")
+        $this->visit('/tournaments/' . $tournament->slug . "/edit")
             ->click('#competitors')
             ->press('generate');
     }
-
-    public function unique_test($numCompetitors, $numGroupsExpected)
-    {
-        $users = factory(User::class, $numCompetitors)->create(['role_id' => Config::get('constants.ROLE_USER')]);
-        if ($users instanceof User) {
-            $user = $users;
-            $users = new Collection();
-            $users->push($user);
-        }
-        $this->makeCompetitors($users);
-        $this->generatePreliminaryTree();
-        $count = Tree::where('championship_id', $this->championship->id)->count();
-        $this->assertTrue($count == $numGroupsExpected);
-    }
-
-
 }
