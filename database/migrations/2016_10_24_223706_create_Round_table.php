c2aebf1367405ddb04f8929d6ee91c21d6425913
julien <julien@t4b.mx>
2017-02-12 17:05:43 -0600

    refactor migrations

---
+++ database/migrations/2016_10_24_223706_create_Round_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoundTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('round', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('championship_id')->unsigned()->index();
+            $table->tinyInteger("area");
+            $table->tinyInteger("order");
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+
+            $table->foreign('championship_id')
+                ->references('id')
+                ->onUpdate('cascade')
+                ->on('championship')
+                ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('round');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
---
+++ database/migrations/2016_11_23_125509_create_Fight_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFightTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fight', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('round_id')->unsigned()->index();
+            $table->foreign('round_id')
+                ->references('id')
+                ->on('round')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('c1')->nullable()->unsigned()->index();
+            $table->foreign('c1')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->integer('c2')->nullable()->unsigned()->index();
+            $table->foreign('c2')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->tinyInteger("area");
+            $table->tinyInteger("order");
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('fight');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
