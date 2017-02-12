b627bd6cd7c7c27ddf9f434370ff55d7bd50b718
julien <julien@t4b.mx>
2017-02-12 17:58:51 -0600

    Now is OK for 4 competitors

--- resources/views/layouts/tree/directElimination.blade.php
+++ resources/views/layouts/tree/directElimination.blade.php
@@ -1,12 +1,13 @@
 <?php
 $directEliminationTree = $championship->tree->map(function ($item, $key) use ($championship) {
+
     if ($championship->category->isTeam()){
 
-        $fighter1 = $item->team1 != null ? $item->team1->name : "Bye";
-        $fighter2 = $item->team2 != null ? $item->team2->name : "Bye";
+        $fighter1 = $item->teams->get(0) != null ? $item->teams->get(0)->name : "Bye";
+        $fighter2 = $item->teams->get(1) != null ? $item->teams->get(1)->name : "Bye";
     }else{
-        $fighter1 = $item->competitors[0] != null ? $item->competitor[0]->user->name : "Bye";
-        $fighter2 = $item->competitors[1] != null ? $item->competitor[1]->user->name : "Bye";
+        $fighter1 = $item->competitors->get(0) != null ? $item->competitors->get(0)->user->name : "Bye";
+        $fighter2 = $item->competitors->get(1) != null ? $item->competitors->get(1)->user->name : "Bye";
     }//    dump([$user1, $user2]);
     return [$fighter1, $fighter2];
 })->toArray();
