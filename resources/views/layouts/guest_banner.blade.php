d2b220d2700632e5a765abf012c2b948126e4fa8
julien <julien@t4b.mx>
2017-04-01 15:21:30 -0600

    remove s from tournaments

--- resources/views/layouts/guest_banner.blade.php
+++ resources/views/layouts/guest_banner.blade.php
@@ -5,23 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description"
-          content="See details of tournament : {{trans_choice('core.tournament',2). ": ".$tournament->name  }} with Kendozone">
+          content="See details of tournament : {{trans_choice('core.tournament',1). ": ".$tournament->name  }} with Kendozone">
     <title>{{ app()->environment()=='local' ? getenv('APP_NAME') : config('app.name') }}</title>
 
-    <meta property="og:title" content="{{trans_choice('core.tournament',2). ": ".$tournament->name  }}"/>
+    <meta property="og:title" content="{{trans_choice('core.tournament',1). ": ".$tournament->name  }}"/>
 
 
-    <meta name="description" content="{{trans_choice('core.tournament',2). ": ".$tournament->name  }}"/>
-    <meta property="og:description" content="{{trans_choice('core.tournament',2). ": ".$tournament->name  }}"/>
+    <meta name="description" content="{{trans_choice('core.tournament',1). ": ".$tournament->name  }}"/>
+    <meta property="og:description" content="{{trans_choice('core.tournament',1). ": ".$tournament->name  }}"/>
     <meta property="og:type" content="website"/>
     <meta property="og:image" content="http://my.kendozone.com/images/banners/KZ-04.jpg"/>
     <meta property="og:image:secure_url" content="https://my.kendozone.com/images/banners/KZ-04.jpg"/>
     <meta property="fb:app_id" content="780774498699579"/>
 
-    <meta name=" twitter:title" content="{{trans_choice('core.tournament',2). ": ".$tournament->name  }}"/>
+    <meta name=" twitter:title" content="{{trans_choice('core.tournament',1). ": ".$tournament->name  }}"/>
     <meta name="twitter:image" content="https://my.kendozone.com/images/banners/KZ-04.jpg"/>
     <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:description" content="{{trans_choice('core.tournament',2). ": ".$tournament->name  }}"/>
+    <meta name="twitter:description" content="{{trans_choice('core.tournament',1). ": ".$tournament->name  }}"/>
 
 
     <!-- Global stylesheets -->
