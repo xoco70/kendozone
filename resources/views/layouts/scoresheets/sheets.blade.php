981f124db4e9e9819e9105c17f4976291040fbb8
julien <julien@t4b.mx>
2017-04-05 18:44:32 -0500

    When single elimination, we put 4 groups per page

--- resources/views/layouts/scoresheets/sheets.blade.php
+++ resources/views/layouts/scoresheets/sheets.blade.php
@@ -1,5 +1,58 @@
-    @forelse($championship->fightersGroups as $group)
-        @include('layouts.scoresheets.sheet', ['group' => $group])
-    @empty
-        {{ trans('core.still_no_scoresheet') }}
-    @endforelse
+{{--TODO This Blade is one of the ugliest in system.... Should Refactor, but don't really know how--}}
+
+@if (sizeof($championship->fightersGroups)==0)
+    {{ trans('core.still_no_scoresheet') }}
+@endif
+@for ( $i=0; $i < sizeof($championship->fightersGroups); $i++ )
+
+    <div class="panel panel-flat page_print">
+        <div class="panel-body">
+            <div class="container-fluid">
+                <?php
+                $group = $championship->fightersGroups->get($i);
+                $fighters = $group->getFighters();
+                ?>
+                <h1 align="center">{{ $tournament->name }} - {{ $roundTitles[$group->round -1 ] }}</h1>
+
+                <hr/>
+                <br/>
+
+                @include('layouts.scoresheets.header', ['championship'=>$championship, 'group'=> $group])
+
+
+                @if (sizeof($fighters) == 2)
+                    @for ($j=0;$j<4;$j++)
+                        <?php
+                        $group = $championship->fightersGroups->get($i);
+                        if ($group != null) {
+                            $fighters = $group->getFighters();
+
+                            if ($j != 3) { // Cancel the last increment
+                                $i++;
+                            }
+
+                        }
+                        ?>
+                        @if ($group != null)
+                            @include('layouts.scoresheets.competitors_direct_elimination', ['fighters'=>$fighters, 'group'=> $group])
+                        @endif
+                    @endfor
+
+
+
+                    <hr/>
+                @else
+                    @include('layouts.scoresheets.competitors', ['fighters'=>$fighters, 'group'=> $group])
+                    <hr/>
+                    @include('layouts.scoresheets.playoff', ['group'=> $group])
+                @endif
+
+
+
+                {{--End Points--}}
+
+
+            </div>
+        </div>
+    </div>
+@endfor
