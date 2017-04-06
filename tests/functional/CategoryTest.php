72af0321c761b25464a2f764bb7c690bd1e14c29
julien <julien@t4b.mx>
2017-04-06 01:52:59 -0500

    Fix create category Test

--- tests/functional/CategoryTest.php
+++ tests/functional/CategoryTest.php
@@ -3,14 +3,12 @@
 use App\Category;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\Config;
 
 
 class CategoryTest extends BrowserKitTest
 {
     use DatabaseTransactions;
-
 
 
     public function setUp()
@@ -25,32 +23,40 @@ class CategoryTest extends BrowserKitTest
      */
     public function create_category()
     {
-        $simpleUser = factory(User::class)->create(['role_id' => Config::get('constants.ROLE_USER')]);
+        $simpleUser = factory(User::class)->create(['locale' => 'en', 'role_id' => Config::get('constants.ROLE_USER')]);
 
         $category = factory(Category::class)->make();
 
         $cat = Category::where('isTeam', '=', $category->isTeam)
             ->where('gender', '=', $category->gender)
-            ->where('gradeCategory', '=', 0)
-            ->select('name')
-            ->first();
+            ->where('gradeCategory', '=', 0);
+
+        if ($category->ageMin != null) $cat = $cat->where('ageMin', $category->ageMin);
+        if ($category->ageMax != null) $cat = $cat->where('ageMax', $category->ageMax);
+        if ($category->gradeMin != null) $cat = $cat->where('gradeMin', $category->gradeMin);
+        if ($category->gradeMax != null) $cat = $cat->where('gradeMax', $category->gradeMax);
+
+        $cat = $cat->select('name')->first();
+
+        if ($cat == null) {
+            $cat = new Category();
+        }
+
+        $cat->isTeam = $category->isTeam;
+        $cat->gender = $category->gender;
+        $cat->ageCategory = $category->ageCategory;
+        $cat->ageMin = $category->ageMin;
+        $cat->ageMax = $category->ageMax;
+        $cat->gradeCategory = $category->gradeCategory;
+        $cat->gradeMin = $category->gradeMin;
+        $cat->gradeMax = $category->gradeMax;
+        $cat->name = $category->buildName();
 
 
-        $data = [
-            'name' => $cat->name,
-            'isTeam' => $category->isTeam,
-            'gender' => $category->gender,
-            'ageCategory' => $category->ageCategory,
-            'ageMin' => $category->ageMin,
-            'ageMax' => $category->ageMax,
-            'gradeCategory' => $category->gradeCategory,
-            'gradeMin' => $category->gradeMin,
-            'gradeMax' => $category->gradeMax,
-
-        ];
+        $data = $cat->toArray();
 
         $this->actingAs($simpleUser, 'api')
-            ->json('POST', '/api/v1/category/create',$data)
+            ->json('POST', '/api/v1/category/create', $data)
             ->seeJson($data)
             ->seeInDatabase('category', $data);
     }
