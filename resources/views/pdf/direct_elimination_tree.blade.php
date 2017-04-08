0332f7b8e185c900efbf3bdfe7df4cea208dfc3a
julien <julien@t4b.mx>
2017-04-08 13:37:51 -0500

    remove footer in Tree PDF

--- resources/views/pdf/direct_elimination_tree.blade.php
+++ resources/views/pdf/direct_elimination_tree.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     @include("layouts.tree.directElimination")
 @stop
-@section('footer')
-    @include('pdf.footer', ['championship' => $championship])
-@stop
+{{--@section('footer')--}}
+    {{--@include('pdf.footer', ['championship' => $championship])--}}
+{{--@stop--}}
 @section('scripts_footer')
     {!! Html::style('css/pages/preliminary_trees.css')!!}
 @stop
