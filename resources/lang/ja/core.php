c37eaed334aabc3f39e1ed23de1a7aa874f1bea6
Julien Cappiello <xoco70@hotmail.com>
2017-05-02 05:37:08 -0500

    add japonese trads

---
+++ resources/lang/ja/core.php
@@ -0,0 +1,398 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    // Models
+
+
+    // Accion
+    'add' => 'agregar',
+    'action' => '作用',
+    'edit' => '編集',
+    'delete' => '削除',
+    'addModel' => '付ける :currentModelName',
+    'editModel' => '編集 :currentModelName',
+    'updateModel' => '修正 :currentModelName',
+    'deleteAllElements' => 'すべての要素を削除する',
+
+
+    'preview' => '下見',
+    'thanks' => 'ありがとう',
+    'or' => '又は',
+    'export_excel' => 'excelにエクスポート',
+    'letsgo' => 'それをやりましょう',
+
+    // Tournament
+    'tournament' => '大会',
+    'newTournament' => '新大会',
+    'createTournament' => '大会を作成する',
+    'date' => '年月日',
+    'eventDate' => '大会の年月日',
+    'account' => 'アカウント',
+    'limitDateRegistration' => '登録の締め切りの日',
+    'limitDate' => '締め切り',
+    'general_data' => '総合案内',
+    'level' => 'レベル',
+    'general_data_step1' => '大会の名前と日付を入力してください',
+
+    'settings' => '入力設定',
+
+    'latitude' => '緯度',
+    'longitude' => '経度',
+    'tournamentType' => '大会種類',
+    'owner' => '主任者',
+    'see_competitors' => '選手',
+    'general' => '一般',
+    'certificates' => '認証',
+    'acredit' => '公認',
+    'broadcast' => '送信',
+    'publish' => '載る',
+    'invite_competitors' => '選手を招待する',
+    'generate_tree' => 'ツリーを作る',
+    'eventDateIni' => '最初の日',
+    'eventDateFin' => '最後の日',
+    'from' => 'から',
+    'to' => 'まで',
+    'competitors_register' => '選手の登録',
+    'configure' => ' コンフィグ',
+
+    'configured' => ' コンフィグした',
+    'add_custom_category' => '範疇を追加する',
+    'rules' => '規則',
+    'promoter' => 'プロモータ',
+    'host_organization' => '司会',
+    'technical_assistance' => '技術的の援助',
+
+
+    // Venue
+    'venue' => '会場',
+    'coords' => '座標',
+    'address' => '住所',
+    'details' => '細部',
+    'city' => '市',
+    'CP' => '郵便番号',
+    'state' => '都道府県',
+    'country' => '国',
+    'location' => '場所',
+
+
+    // Nivel
+    'local' => '地元',
+    'district' => '区',
+    'level_city' => '都市',
+    'regional' => '地域的',
+    'level_state' => '都道府県のレベル',
+    'national' => '全国',
+    'international' => '国際',
+
+
+    // User
+    'user' => 'ユーザ',
+    'name' => '名前',
+    'username' => 'ユーザー名',
+    'profile' => 'プロファイル',
+    'grade' => '段位',
+    'avatar' => 'アバター',
+    'email' => 'メール',
+    'email_desc' => 'アカウントのメール',
+    'role' => '役割',
+    'firstname' => '名前',
+    'lastname' => '名字',
+    'password' => 'パスワード',
+    'newpassword' => '新パスワード',
+    'conewpassword' => 'パスワードを確認して',
+    'without_grade' => '段位ないで',
+    'left_password_blank' => 'パスワードを変更したくない場合は空欄にしてください',
+    'remove' => '除く',
+
+
+    // Competitor
+    'competitor' => '選手',
+    'add_competitor' => '選手を足す',
+    'add_competitor_to_category' => '選手をカテゴリに足す: category',
+    'select_competitor_categories' => '選手のカテゴリを選択してください',
+    'select_tournament_categories' => '大会のカテゴリを選択する',
+    'paid' => '支払い済み',
+
+
+    // Invitation
+
+    'invitation' => '招待',
+    'Tournament_invitations' => '選手を招待する',
+    'invite_send' => '大会に選手を招待する:: tournament',
+    'recipients' => '受取人',
+    'invite_recipients' => '全ての受取人の名前を書いてコンマで分別してください',
+    'invite_message' => '招待のメッセージ',
+    'invite_template' => "大会を登録のためにリンクをクリックしてください:: tournament \ n: link",
+    'send_invites' => '招待状を送る',
+    'invite_with_link' => 'そのリンクの選手を招待する',
+    'no_tournament_registered_yet' => 'まだ大会に登録されていません',
+    'no_invitation_yet' => 'まだ大会に招待されていません',
+
+    'confirmed' => '確認した',
+    'used' => '中古',
+    'results' => '結果',
+    'latest_computers' => '最新の選手',
+    'organizer' => '主催者',
+    'select_categories_to_register' => '登録するカテゴリを選択する',
+
+    '2 _ choices_to_invite' => '選手を招待するのためには二つの仕方あります:',
+    'invite_by_commas' => 'メールで招待することができます',
+    'invite_by_excel' => 'excelで招待することができます',
+
+
+    // Messages
+    'all_categories_not_configured' => 'まずすべてのカテゴリを最初に設定する必要があります.',
+
+
+    // Share
+    'share' => 'シェア',
+    'share_link' => '共有リンク',
+
+    // Dashboard
+
+    'logs' => 'ログ',
+    'dashboard' => 'ダッシュボード',
+    'logout' => 'ログアウト',
+    'home' => 'ホーム',
+// 'personalinfo' => '自分のインフォーメーション',
+    'changepassword' => 'パスワードを変更する',
+    'savechanges' => '変更を保存する',
+
+    'all_fields_required' => 'すべてのフィールドは必須です',
+    'error' => '過失',
+    'warning' => '注意',
+    'success' => '成功',
+    'info' => 'インフォーメーション',
+    'information' => 'インフォーメーション',
+    'operation_successful' => '成功した操作',
+    'operation_failed' => '操作に失敗しました',
+    'save' => 'セーブ',
+    'help' => '手伝',
+    'type' => '種類',
+    'open' => '開',
+    'yes' => 'はい',
+    'no' => 'いいえ',
+    'forbidden' => '禁じられた',
+
+
+    // Left menu
+    'lastlogin' => '先のログイン’',
+//    'users' => 'ユーザー達',
+//    'statistics' => '統計',
+
+    // Right Menu
+
+    'sumary' => '概略',
+    'see_trees' => 'ツリーをみる',
+    //Settings
+
+    'social_networks' => 'ソーシャルネットワーク',
+
+
+    // Dashboard
+    'welcome' => 'ようこそ',
+    'welcome_text1' => 'ほとんどそこに！あなたはまだAppを楽しむためにいくつかのステップが必要です',
+    'welcome_text2' => '何をしたいですか?',
+
+    'still_no_tournament' => 'まだ大会に登録されていません',
+    'still_no_open_tournament' => 'まだお国のオープン大会はありません',
+    'create_new_tournament' => '新しい大会を作成する',
+    'congigure_categories' => 'セットアップカテゴリ',
+    'see_open_tournaments' => 'オープン大会を見る',
+    'open_tournaments_in_your_country' => 'お国のオープン大会',
+    'soon' => 'すぐ',
+    'configured_full' => ' コンフィグ した',
+    'see_all' => 'すべてを見る',
+    'see_more' => 'もっと見る',
+    'no_tournament_created_yet' => 'まだ大会に登録していません',
+    'no_tournament_deleted_yet' => 'まだ大会を削除していません',
+
+    'join_tournament' => '既存の大会に参加する',
+    'tournaments_registered' => '大会記録',
+    'tournaments_created' => '大会が作成されました',
+    'tournaments_deleted' => '大会が削除されました',
+    'numbers' => '数',
+    'created' => '作った',
+    'participations' => '出たの',
+    'past' => '先の',
+    'next' => '次の',
+
+
+    // Logs
+
+    'object_type' => 'オブジェクト',
+    'operation_type' => '操作',
+    'object_id' => 'オブジェクトID',
+    'created_at' => '作成済み',
+    'updated_at' => '更新済み',
+    'old_value' => '古い値',
+    'new_value' => '新しい値',
+
+
+    // Teams
+
+    'team' => 'チーム',
+    'no_team_yet' => 'まだこの大会に登録されていないチーム',
+    'add_new_team' => '大会を追加',
+    'registered_team' => 'チーム登録済み',
+
+    // Federations
+    'federation' => '連盟',
+    'federation.president' => '連盟長',
+    'federation.vicepresident' => '副長',
+    'federation.secretary' => '秘書',
+    'federation.treasurer' => '会計',
+    'federation.admin' => '管理者',
+    'federation.address' => '住所',
+    'federation.phone' => '電話',
+    'federation.no_user_in_this_country' => 'この国に登録されているユーザーはいません',
+    'no_federation' => '連盟なし ',
+
+    // Associations
+    'association' => '付き合い',
+    'association.president' => '会長',
+    'association.address' => '住所',
+    'association.phone' => '電話',
+    'association.no_user_in_this_country' => 'この国に登録されているユーザーはいません',
+    'association.add' => '付き合いを足す',
+    'no_association_yet' => 'まだこの連合には関連がありません',
+    'add_new_association' => '新しい関連付けを追加する',
+    'no_association' => '関連付けなし ',
+
+    // Clubs
+    'club' => 'クラブ',
+    'club.president' => '会長',
+    'club.address' => '住所',
+    'club.phone' => '電話',
+    'club.no_user_in_this_country' => 'この国に登録されているユーザーはいません',
+
+
+//   どこで訓練しますか？',
+    'select_field' => 'オプションを選択する',
+    'no_association_available' => 'この連合には関連はありません',
+    'no_club_available' => 'この協会にはクラブがありません',
+
+
+    //Excel
+
+    'import_excel' => 'excelからデータをインポートする',
+    'upload_file_to_csv_format' => 'ファイルは.csv形式である必要があります。',
+    'how_to_save_to_csv' => 'excelでは,ファイル>名前を付けて保存で保存することができます。次に,「ファイル形式」で「.csv」を選択します。',
+    'bulk_upload' => '競合他社にExcelファイルを招待する',
+    'download_layout' => 'レイアウトファイル',
+
+    // Trees
+    'tree' => 'ツリー',
+    'no_generated_tree' => 'まだこの選手権ではツリーが生成されていません',
+    'document' => '書類',
+    'print' => 'プリント',
+
+    // Fights
+    'fight' => '立会',
+    'no_fight_list' => 'まだこの選手権の試合リストはありません',
+
+    // Share
+
+    'register_in_tournament' => 'トーナメントに登録する',
+
+
+    //Common
+    'contact_us' => 'contactanos',
+    'optional' => 'opcional',
+
+    //Tournament
+    'invite_competitors_with_email' => 'invita por Email',
+    'invite_competitors_with_fb' => 'invita con Facebook',
+    'admin_tournaments' => 'administrar torneos',
+
+
+    // Competitor
+    'add_more_competitors' => 'agregar más competidores',
+    'competitor_name' => 'nombre del Competidor',
+    'remove_competitor' => 'eliminar competidor',
+    'add_competitor_text1' => 'si no ingresa el correo electrónico del competitor, Kendozone generará uno de forma automática',
+    'add_competitor_text2' => 'nota que en este caso, el competitor tendrá un ID diferente ID para cada categoría del torneo',
+    'add_competitor_text3' => 'Para tener ID unicos, ingresa el email, o ',
+
+
+    // Invitation
+
+    'invitaion' => 'invitación|Invitaciones',
+    'tournament_invitations' => 'invita competidores',
+    'latest_competitors' => 'Últimos registros',
+
+    '2_choices_to_invite' => 'Tienes 2 opciones para invitar competidores:',
+
+
+    // Dashboard
+
+
+    // Left menu
+    'users' => 'Users',
+    'statistics' => 'Statistics',
+
+    'see_less' => 'See Less',
+
+    'impersonate' => 'Impersonate',
+    'leave_impersonation' => 'Leave Impersonation',
+
+    // Teams
+
+    'team_name' => 'Team name',
+
+    // Federations
+    'federation_not_in_list' => 'Your federation is not in the list?',
+
+
+    // Associations
+    'association_not_in_list' => 'Your association is not in the list?',
+
+    // Clubs
+    'add_new_club' => 'Add a new club',
+
+    'where_do_you_practice' => '¿Where do you train?',
+    'club_not_in_list' => 'Your club is not in the list?',
+
+
+    // Share
+    'share_tournament' => 'Share tournament',
+    'check_the_tournament' => 'Check the tournament:',
+
+
+    'confirm_name' => 'Confirm your full name',
+    'about' => "About Kendozone",
+
+    'share_to_facebook' => 'Share to Facebook',
+    'share_to_twitter' => 'Share to Twitter',
+    'share_to_googleplus' => 'Share to Google +',
+
+
+    // Score Sheet
+    'scoresheet' => 'Scoring Sheet | Scoring Sheets',
+    'championship' => 'Championship',
+    'shiajo' => 'Shiajo',
+    'combat' => 'Shiai',
+    'group' => 'Group',
+    'writer' => 'Writer',
+    'playoff' => 'Play off',
+    'points' => 'Points',
+    'points_abrev' => 'Pts',
+    'clasify' => 'Clasify',
+    'time' => 'Time',
+    'table_leader' => 'Table Leader',
+    'still_no_scoresheet' => 'Still no Score sheet generated',
+    'penalization' => 'Hans.',
+    'point' => 'Point',
+    'winner' => 'Winner',
+];
