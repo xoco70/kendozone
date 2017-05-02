f459636dc22a8c17612999edbf7fcbc909942296
Julien Cappiello <xoco70@hotmail.com>
2017-05-02 16:10:04 -0500

    backup weekly ( just to see if affects invoice )

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('02:00');
+        $schedule->command('backup:clean')->weekly()->at('01:00');
+        $schedule->command('backup:run')->weekly()->at('02:00');
     }
 
     /**
