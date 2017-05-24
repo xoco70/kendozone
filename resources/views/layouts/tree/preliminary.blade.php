d0c0a370ff9f50be8d28710bd97a7079a74fcc51
Julien Cappiello <xoco70@hotmail.com>
2017-05-24 15:24:12 -0500

    update

--- resources/views/layouts/tree/preliminary.blade.php
+++ resources/views/layouts/tree/preliminary.blade.php
@@ -24,40 +24,18 @@
 
             @foreach($groupsByArea as $group)
                 <?php
-                if ($championship->category->isTeam) {
-                    $fighter1 = $group->teams->get(0) != null ? $group->teams->get(0)->name : '';
-                    $fighter2 = $group->teams->get(1) != null ? $group->teams->get(1)->name : '';
-                    $fighter3 = $group->teams->get(2) != null ? $group->teams->get(2)->name : '';
-                    $fighter4 = $group->teams->get(3) != null ? $group->teams->get(3)->name : '';
-                    $fighter5 = $group->teams->get(4) != null ? $group->teams->get(4)->name : '';
-                } else {
-                    $fighter1 =$group->competitors->get(0) != null ? $group->competitors->get(0)->user->name : '';
-                    $fighter2 = $group->competitors->get(1) != null ? $group->competitors->get(1)->user->name : '';
-                    $fighter3 = $group->competitors->get(2) != null ? $group->competitors->get(2)->user->name : '';
-                    $fighter4 = $group->competitors->get(3) != null ? $group->competitors->get(3)->user->name : '';
-                    $fighter5 = $group->competitors->get(4) != null ? $group->competitors->get(4)->user->name : '';
-
-                }
+                $names = $group->fighters()->map->name;
+                $letter = 'a';
                 ?>
-                    <tr>
-                        <td class="p-10">{{$group->area}}</td>
-                        <td class="p-10">a</td>
-                        <td class="p-10">{{ $fighter1 }}</td>
-                        <td class="p-10">b</td>
-                        <td class="p-10">{{ $fighter2 }}</td>
-                        <td class="p-10">c</td>
-                        <td class="p-10">{{ $fighter3 }}</td>
-
-                        @if ($championship->settings!= null && $championship->settings->preliminaryGroupSize > 3)
-                            <td class="p-10">d</td>
-                            <td class="p-10">{{ $fighter4 }}</td>
-                        @endif
-                        @if ($championship->settings!= null && $championship->settings->preliminaryGroupSize > 4)
-                            <td class="p-10">e</td>
-                            <td class="p-10">{{ $fighter5 }}</td>
-                        @endif
-                    </tr>
+                <tr>
+                    <td class="p-10">{{$group->area}}</td>
+                    @foreach ($names as $name)
+                        <td class="p-10">{{ $letter++ }}</td>
+                        <td class="p-10">{{ $name }}</td>
+                    @endforeach
+                </tr>
             @endforeach
+
         </table><br/>
     @endforeach
 </div>
