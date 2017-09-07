aa608058abc439b81a23d3b7bc8e0f0aea66d344
Julien Cappiello <xoco70@hotmail.com>
2017-09-07 22:15:06 +0200

    simplify views

--- resources/views/layouts/scoresheets/sheets.blade.php
+++ resources/views/layouts/scoresheets/sheets.blade.php
@@ -1,48 +1,33 @@
 @if (sizeof($championship->fightersGroups)==0)
     {{ trans('core.still_no_scoresheet') }}
 @endif
-@for ( $i=0; $i < sizeof($championship->fightersGroups); $i++ )
+@foreach ($championship->fightersGroups as $group )
+    @if ($group->fights[0]->shouldBeInFightList(true) ) {{--if it has short_id, it must be in the list, otherwise remove it--}}
+        <div class="panel panel-flat page_print">
+            <div class="panel-body">
+                <div class="container-fluid">
+                    <?php
+                    $fighters = $group->getFightersWithBye();
+                    ?>
+                    <h1 align="center">{{ $tournament->name }} - {{ $roundTitles[$group->round -1 ] }}</h1>
 
-    <div class="panel panel-flat page_print">
-        <div class="panel-body">
-            <div class="container-fluid">
-                <?php
-                $group = $championship->fightersGroups->get($i);
-                $fighters = $group->getFightersWithBye();
-                ?>
-                <h1 align="center">{{ $tournament->name }} - {{ $roundTitles[$group->round -1 ] }}</h1>
+                    <hr/>
+                    <br/>
 
-                <hr/>
-                <br/>
+                    @include('layouts.scoresheets.header', ['championship'=>$championship, 'group'=> $group])
 
-                @include('layouts.scoresheets.header', ['championship'=>$championship, 'group'=> $group])
-
-
-                @if (sizeof($fighters) == 2)
-                    @for ($j=0;$j<4;$j++)
-                        <?php
-                        $group = $championship->fightersGroups->get($i);
-                        if ($group != null) {
-                            $fighters = $group->getFightersWithBye();
-
-                            if ($j != 3) { // Cancel the last increment
-                                $i++;
-                            }
-
-                        }
-                        ?>
-                        @if ($group != null)
+                    {{--  Maybe we will have a problem with Cancelling the last increment --}}
+                    @if (sizeof($fighters) == 2)
+                        @if ($group != null )
                             @include('layouts.scoresheets.competitors_direct_elimination', ['fighters'=>$fighters, 'group'=> $group])
                         @endif
-                    @endfor
-                    <hr/>
-                @else
-                    @include('layouts.scoresheets.competitors', ['fighters'=>$fighters, 'group'=> $group])
-                    <hr/>
-                    @include('layouts.scoresheets.playoff', ['group'=> $group])
-                @endif
-                {{--End Points--}}
+                    @else
+                        @include('layouts.scoresheets.competitors', ['fighters'=>$fighters, 'group'=> $group])
+                        @include('layouts.scoresheets.playoff', ['group'=> $group])
+                    @endif
+                    {{--End Points--}}
+                </div>
             </div>
         </div>
-    </div>
-@endfor
+    @endif
+@endforeach
