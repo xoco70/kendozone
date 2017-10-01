779806021967b63f44691cc66d2f98a438be7b4b
Julien Cappiello <contact@kendozone.com>
2017-10-01 21:24:22 +0200

    refactor avatar upload

--- app/Avatar.php
+++ app/Avatar.php
@@ -3,49 +3,21 @@
 namespace App;
 
 use DateTime;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Intervention\Image\ImageManagerStatic as Image;
 
 
 class Avatar
 {
-    /**
-     * Upload Pic
-     * @param $data
-     * @return array
-     */
-    public static function uploadPic($data)
-    {
-        $file = array_first($data, null);
-        if ($file != null && $file->isValid()) {
-            $destinationPath = config('constants.RELATIVE_AVATAR_PATH');
-            $fileName = self::createUniqueFileName($file);
-            if (!$file->move($destinationPath, $fileName)) {
-                echo "No se pudo mover";
-                flash()->error("Upload error, please try again");
-                return $data;
-            }
-            $data['avatar'] = $fileName;
-            // Redimension and pic
-            static::resizePicAndSave($destinationPath, $fileName);
-            Auth::user()->avatar = $fileName;
-            Auth::user()->save();
-            return $data;
 
-
-        }
-        echo "File is not valid";
-        return $data;
-    }
 
     /**
-     * @param $destinationPath
      * @param $fileName
+     * @return string
      */
-    public static function resizePicAndSave($destinationPath, $fileName)
+    public static function resize($fileName)
     {
-        $img = Image::make($destinationPath . $fileName);
+        $img = Image::make($fileName);
         $width = null;
         $height = 200;
 
@@ -58,7 +30,8 @@ class Avatar
             $constraint->aspectRatio();
         });
 
-        $img->save($destinationPath . $fileName);
+        $img->save($fileName);
+        return $fileName;
     }
 
     /**
--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 
-use App\Avatar;
-use Illuminate\Http\Request;
-
 class UserAvatarController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
      */
-    public function store(Request $request)
+    public function store()
     {
-        $data = $request->except('_token');
-        $data = Avatar::uploadPic($data);
-        return $data;
+        $file = basename(
+            request()
+                ->file('file')
+                ->store(config('constants.RELATIVE_AVATAR_PATH'), 'public')
+        );
 
+        auth()->user()->update(['avatar' => $file]);
+        return response([], 204);
     }
 }
