baa8d26a66d1b124fa2382fe66a57a9dae5097c6
Julien Cappiello <contact@kendozone.com>
2017-10-01 21:50:40 +0200

    fix avatar

--- resources/views/layouts/avatar.blade.php
+++ resources/views/layouts/avatar.blade.php
@@ -1,15 +1,13 @@
 <?php
 $user = Auth::user();
-//dd(App\User::find(1)->avatar);
 if (
     isset($user) &&
     $user != null &&
     $user->avatar != null) {
-    $img = $user->avatar;
+    $img = asset($user->avatar);
 } else {
     $img = Avatar::create($user->email)->toBase64();
 }
-//dd(Auth::user())
 ?>
 <img src="{!! $img !!}" alt="kendozone_avatar">
 
