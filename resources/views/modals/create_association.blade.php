bcc42ae758d770dd0486ccd4c55c9eb38e62a207
Julien Cappiello <contact@kendozone.com>
2018-02-02 19:52:14 +0100

    Fix #19

--- resources/views/modals/create_association.blade.php
+++ resources/views/modals/create_association.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header bg-primary">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h6 class="modal-title">{{ trans('core.add_new_association') }}</h6>
+                <h6 class="modal-title">{{ trans('structures.add_new_association') }}</h6>
             </div>
 
             <div class="panel-body">
