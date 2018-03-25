7a3b7c53df4908452ec9d4219761a5034fad2836
Julien Cappiello <contact@kendozone.com>
2018-03-25 17:26:29 +0200

    all things but dockefile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-composer install --no-interaction
-npm install
 php artisan key:generate
+echo "Waiting for database connection..."
+sleep 10
 php artisan migrate:fresh --seed
+#npm run dev
 #php artisan serve --host=0.0.0.0 --port=80
 php-fpm
 
