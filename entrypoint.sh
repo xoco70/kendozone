579f9b05448ce6c20105f1926448a0b69d032cec
julien.cappiello <externe.julien.cappiello@thales-services.fr>
2018-03-28 16:38:11 +0200

    minimalistic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-
-php artisan key:generate
+php artisan serve --host=0.0.0.0 --port=$APP_PORT &
 echo "Waiting for database connection..."
 sleep 10
 php artisan migrate:fresh --seed
-#npm run dev
-#php artisan serve --host=0.0.0.0 --port=80
-php-fpm
 
